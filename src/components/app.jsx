fad58a99d2ad91b17dc1f893b54235a9855a18c4
Grant Holley <gvholley@gmail.com>
2020-11-18 13:03:00 +0900

    basics added

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from "react";
+import FlatList from "../containers/flat_list";
+import Map from "../containers/map";
 
-import FlatList from '../containers/flat_list';
-import Map from '../containers/map';
-
-class App extends Component {
-  render() {
-   return (<div className="row">
+const App = () => {
+  return (
+    <div className="row">
       <FlatList />
       <Map />
     </div>
-    );
-  }
-}
+  );
+};
 
 export default App;
