9ccae7cf55e4967706d44da2c493757005aab5a7
Grant Holley <gvholley@gmail.com>
2020-11-19 11:47:56 +0900

    Applied middleware

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { logger } from 'redux-logger';
+import App from './components/app';
 
 import '../assets/stylesheets/application.scss';
 import flatsReducer from './reducers/flats_reducer';
 import selectedFlatReducer from './reducers/selected_flat_reducer';
+
 const reducers = combineReducers ({
   flats: flatsReducer,
   selectedFlat: selectedFlatReducer
 });
 
-import App from './components/app';
+const middlewares = applyMiddleware(logger);
 
 
 const root = document.getElementById('root');
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={createStore(reducers, {}, middlewares)}>
     <App />
   </Provider>,
   root);
